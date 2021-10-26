ef2f682939879c54b315cc85d88fe35c35287ed7
Christian <xtianluz@gmail.com>
2021-10-26 20:53:47 +0800

    connected to mongodb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
+const dotenv = require('dotenv')
+dotenv.config()
+const { MongoClient } = require('mongodb');
+const {DB_URI, DB_NAME} = process.env
+
+
 
 const books = [
     {
@@ -36,16 +42,30 @@ const typeDefs = gql`
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+      books: (root, data, context) => {
+          console.log(context)
+          return books;
+      },
     },
   };
-  
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+const start = async () => {
+    const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db(DB_NAME)
 
+    const context = {
+        db,
+    }
+
+    // The ApolloServer constructor requires two parameters: your schema
+    // definition and your set of resolvers.
+    const server = new ApolloServer({ typeDefs, resolvers, context });
+
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+    });
+}
+
+start()
